6d0ceb63b1ac3dd96649d16a644139d5f24f6397
CRBroughton <crbroughton@posteo.uk>
2021-07-19 13:08:20 +0100

    added styling to Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -15,4 +15,39 @@ export default {
 </script>
 
 <style>
+/* ---------------------------------- */
+/* BASIC SETUP */
+/* ---------------------------------- */
+
+/*Include padding and border in the element's total width and height*/
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+* {
+  margin: 0; /*remove all margin and padding
+                from elements at the start*/
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+html,
+body {
+  background-color: #fff;
+  color: #555;
+  font-family: "Open-Sans", "Arial", sans-serif;
+  font-weight: 300;
+  font-size: 20px;
+  text-rendering: optimizeLegibility;
+  overflow-x: hidden; /*stops overflow on the x axis, 
+                        usually shows as a white strip
+                        from top to bottom of page*/
+}
 </style>
