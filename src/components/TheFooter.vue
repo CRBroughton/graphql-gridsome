ba2cdbd7905783e5cd16866b78dcc2fadd82dfa5
CRBroughton <crbroughton@posteo.uk>
2021-07-19 13:11:21 +0100

    created TheFooter.vue

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col mx-auto my-4 text-xs text-gray-300">
+    <p>
+      Made with <a href="https://gridsome.org/">Gridsome</a> |
+      <a href="https://vuejs.org/">Vue</a> |
+      <a href="https://graphql.org/">GraphQL</a>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
