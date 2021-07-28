c20967c48cb1f6980b74823ce991e75caa4b1d75
CRBroughton <crbroughton@posteo.uk>
2021-07-28 14:54:08 +0100

    added load more button added limit to launches to 4 per load

--- src/components/BaseLaunch.vue
+++ src/components/BaseLaunch.vue
@@ -2,14 +2,15 @@
 <template>
   <div class="grid grid-cols-1 max-w-screen-sm min-w-min">
     <div
-      v-for="launch in launches[0]"
-      :key="launch.id"
+      v-for="(launch, index) in paginatedOrders"
+      :key="index"
       class="text-white border-4 border-gray-200 transition duration-500 ease-in-out transform hover:scale-105 hover:border-gray-400 cursor-pointer max-h-screen m-4"
       @click="toggleModal"
     >
         <base-info-modal :launch="launch" :showModal="showModal" class="absolute"/>
         <img v-lazy="launch.links.flickr_images[0]" />
       </div>
+      <button @click="loadMore" v-if="page * maxLaunchesPerPage" >Load More</button>
     </div>
 </template>
 
@@ -24,7 +25,10 @@ export default {
   data() {
     return {
       launches: [],
+      page: 1,
+      maxLaunchesPerPage: 4,
       showModal: false,
+      componentsLoaded: false
     };
   },
   async mounted() {
@@ -37,10 +41,22 @@ export default {
       (iterator) => iterator.links.flickr_images.length > 0
     );
     this.launches.push(filteredLaunches);
+    this.componentsLoaded = true;
+  },
+  computed: {
+    paginatedOrders() {
+      if (!this.componentsLoaded) {
+        return null;
+      }
+      return this.launches[0].slice(0, this.page * this.maxLaunchesPerPage);
+    }
   },
   methods: {
     toggleModal() {
       this.showModal = !this.showModal;
+    },
+    loadMore() {
+      this.page += 1;
     }
   }
 };
